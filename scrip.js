863e4c08e820d569a8c4d757aa3b00ad164a68fd
bekzodd746 <bekzodxoliqov746@gmail.com>
2025-07-21 13:05:48 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,11 @@
+let nam = prompt(`Write your name` )
+
+let letter= prompt(`Write your letter` )
+
+ if(nam.toUpperCase(letter)){
+    console.log('bor');
+    
+ }else{
+    console.log(`yoq`);
+
+ }
